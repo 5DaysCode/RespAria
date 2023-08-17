5a6ccc2912bf14ebab7f1d2eab2fe6e5d6688aab
5DaysCode <5DaysCode@gmail.com>
2023-08-17 11:42:07 +0200

    added login component with WCAG and WAI-AREA  funtionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
-  title = 'RespAria';
+  showNavbarFooter: boolean = true;
+
+  constructor(private router: Router) {
+    //Listening to route chanegs
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showNavbarFooter = event.url !== '/login'; //dont sow navbar
+      }
+    });
+  }
 }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.less'],
+})
+export class LoginComponent {
+  onSubmit() {
+    console.log('Form submitted');
+  }
+}
